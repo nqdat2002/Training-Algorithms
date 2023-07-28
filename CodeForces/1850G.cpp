d33b0b0f5b446813ee176b20645139330cb355dd
nqdat02 <ngquocdat.work@gmail.com>
2023-07-28 19:50:42 +0700

    Update 1850G

--- CodeForces/1850G.cpp
+++ CodeForces/1850G.cpp
@@ -22,16 +22,32 @@ const long long MM = 1LL * MOD * MOD;
 
 int main() {
     int t; cin >> t;
-    while (t--){
-        int n; cin >> n;
-        int cnt = 0;
-        map<int, int> x, y, dx, dy;
-        for(int i = 0; i < n; ++i){
-        	int a, b; cin >> a >> b;
-        	x[a] ++;
-        	y[b] ++;
-        	if(abs(a) == abs(b)) cnt ++;
+    while (t--) {
+        ll n;
+        cin >> n;
+        map<ll, ll> up, side, diag1, diag2;
+        ll ans = 0;
+        for (int i = 0; i < n; i++){
+            ll x, y;
+            cin >> x >> y;
+            up[x]++;
+            side[y]++;
+            diag1[x - y]++;
+            diag2[x + y]++;
         }
+        for (auto x : up){
+            ans += x.second * (x.second - 1);
+        }
+        for (auto x : side){
+            ans += x.second * (x.second - 1);
+        }
+        for (auto x : diag1){
+            ans += x.second * (x.second - 1);
+        } 
+        for (auto x : diag2){
+            ans += x.second * (x.second - 1);
+        }
+        cout << ans << endl;
     }
     return 0;
 }
