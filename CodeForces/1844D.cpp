28d1937b167eddbd6b153f97890a318eb684886c
nqdat02 <ngquocdat.work@gmail.com>
2023-08-01 12:21:19 +0700

    Codeforces Round 884

--- CodeForces/1844D.cpp
+++ CodeForces/1844D.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 char s[1000001];
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
