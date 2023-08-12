49288c06f80b4b0d50bce117827a40c3ac6ae33a
nqdat02 <ngquocdat.work@gmail.com>
2023-08-13 00:27:30 +0700

    Codeforces Round 890 (Div. 2)

---
+++ CodeForces/1856A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> p;
+typedef vector<vector<int> > vt;
+typedef vector<pair<int, int> > vp;
+const ll mod = 1e9 + 7;
+const int oo = 1e6 + 7;
+
+#define f first
+#define s second
+#define pb push_back
+#define ep emplace_back
+#define sz(a) (int) a.size()
+#define ms(s, n) memset(s, n, sizeof(s))
+#define present(t, x) (t.find(x) != t.end())
+#define all(a) (a.begin(), a.end())
+#define For(i, l, r) for (int i = l; i <= r; i++)
+#define Fod(i, r, l) for (int i = r; i >= l; i--)
+#define fillchar(a, x) memset(a, x, sizeof (a))
+#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void FileIO() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+}
+
+int main(int argc, char const *argv[]) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t; cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		int a[n];
+		for (auto &x : a) cin >> x;
+		if (is_sorted(a, a + n)) {
+			cout << 0 << endl;
+			continue;
+		}
+		int res = -1;
+		for (int i = n - 1 ; i >= 1; --i) {
+			if (a[i] >= a[i - 1]) continue;
+			else {
+				res = i;
+				break;
+			}
+		}
+		cout << *max_element(a, a + res);
+		cout << endl;
+	}
+	return 0;
+}
