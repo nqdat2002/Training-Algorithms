32410754de5ea0ec6f3df4b01fec1890897a508b
nqdat02 <ngquocdat.work@gmail.com>
2023-08-15 21:03:21 +0700

    Update Hexgame.cpp

--- Thuat toan nang cao/Hexgame.cpp
+++ Thuat toan nang cao/Hexgame.cpp
@@ -25,7 +25,7 @@ const int oo = 1e6 + 7;
 #define fillchar(a, x) memset(a, x, sizeof (a))
 #define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void FileIO() {
+void FileIO(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 }
@@ -36,70 +36,70 @@ int drx[] = {1, 5, 2, 4, 9, 6, 3, 8, 10, 7};
 int dln[] = {2, 6, 3, 1, 5, 9, 7, 4, 8, 10};
 int drn[] = {1, 3, 7, 4, 2, 6, 10, 8, 5, 9};
 
-string Drt(string s, int b[], int n) {
+string Drt(string s, int b[], int n){
     string res = "";
-    for (int i = 0; i < n; i ++) res += s[b[i] - 1];
+    for(int i = 0; i < n; i ++) res += s[b[i] - 1];
     return res;
 }
 map<string, int> v, u;
-void BFS(string s) {
+void BFS(string s){
     v.clear();
     queue<string> q;
     q.push(s);
     v[s] = 1;
-    while (!q.empty()) {
+    while(!q.empty()){
         string c = q.front(); q.pop();
         string a = Drt(c, dln, 10);
-        if (v[a] == 0) {
+        if(v[a] == 0){
             q.push(a);
             v[a] = v[c] + 1;
         }
 
         string b = Drt(c, drn , 10);
-        if (v[b] == 0) {
+        if(v[b] == 0){
             q.push(b);
             v[b] = v[c] + 1;
         }
-        if (v[q.back()] >= 15) break;
+        if(v[q.back()] >= 15) break;
     }
 }
 
-void BFS2(string s) {
+void BFS2(string s){
     u.clear();
     queue<string> q;
     q.push(s);
     u[s] = 1;
-    while (!q.empty()) {
+    while(!q.empty()){
         string c = q.front(); q.pop();
-        if (v[c]) {
+        if(v[c]){
             cout << u[c] + v[c] - 2 << endl;
             return;
         }
         string a = Drt(c, dlx, 10);
-        if (u[a] == 0) {
+        if(u[a] == 0){
             q.push(a);
             u[a] = u[c] + 1;
         }
 
         string b = Drt(c, drx, 10);
-        if (u[b] == 0) {
+        if(u[b] == 0){
             q.push(b);
             u[b] = u[c] + 1;
         }
-        if (u[q.back()] >= 15) break;
+        if(u[q.back()] >= 15) break;
     }
 }
 
-int main() {
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    
     int t; cin >> t;
-    while (t--) {
+    while(t--){
         string s = "";
-        BFS("1238994765");
-        for (int i = 0; i < 10; i ++) { char x; cin >> x; s += x; }
+        BFS("1238004765");
+        for(int i = 0; i < 10; i ++) { char x; cin >> x; s += x; }
         BFS2(s);
     }
     return 0;
